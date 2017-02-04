9e975036d11eb550864906287ab0861ae069df16
Jess Forrest <jessforrest@users.noreply.github.com>
2017-02-04 00:51:30 -0600

    Initial check-in.
    -Added SignIn
    -Added GetList

---
+++ OurGroceries.Api/OurGroceries.Api.Test/ClientTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OurGroceries.Api;
+using OurGroceries.Api.Entities;
+using Newtonsoft.Json;
+
+namespace OurGroceries.Test
+{
+    [TestClass]
+    public class ClientTest
+    {
+        const string emailAddress = "** YOUR EMAIL ADDRESS **";
+        const string password = "** YOUR PASSWORD **";
+
+        [TestMethod]
+        public void Signup_HappyPath()
+        {
+            using (Client client = new Client())
+            {
+                var actual = client.SignIn(emailAddress, password);
+
+                Assert.IsTrue(!string.IsNullOrEmpty(actual));
+            }            
+        }
+
+        [TestMethod]
+        public void GetList_HappyPath()
+        {
+            using (Client client = new Client())
+            {
+                var shoppingTeamJson = client.SignIn(emailAddress, password);
+                var actual = string.Empty;
+
+                if(!string.IsNullOrEmpty(shoppingTeamJson))
+                {
+                    var shoppingTeam = JsonConvert.DeserializeObject<ShoppingTeam>(shoppingTeamJson);
+
+                    if (shoppingTeam == null || shoppingTeam.shoppingLists.Count == 0) Assert.Fail();
+
+                    actual = client.GetList(listId: shoppingTeam.shoppingLists[0].id, teamId: shoppingTeam.teamId);
+                }
+
+                Assert.IsFalse(string.IsNullOrEmpty(actual));
+            }
+        }
+    }
+}
