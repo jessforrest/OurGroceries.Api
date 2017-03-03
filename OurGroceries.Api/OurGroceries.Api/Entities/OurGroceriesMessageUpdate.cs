afe98d07f4d3f8394b7739b77b8f36e3d4cf8832
Jess Forrest <github.10.i3adme@spamgourmet.com>
2017-03-02 22:24:17 -0600

    -added missing class.

---
+++ OurGroceries.Api/OurGroceries.Api/Entities/OurGroceriesMessageUpdate.cs
@@ -0,0 +1,8 @@
+﻿namespace OurGroceries.Api.Entities
+{
+    public class OurGroceriesMessageUpdate : OurGroceriesMessage
+    {
+        public string itemId { get; set;}
+        public string crossedOff { get; set;}
+    }
+}
